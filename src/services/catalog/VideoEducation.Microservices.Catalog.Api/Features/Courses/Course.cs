77f345c851efda8d8796f8542336651e5c70e110
Emre Yüce <emre.yuce@simetri8.com>
2024-11-13 15:29:01 +0300

    * Feature entity created

--- src/services/catalog/VideoEducation.Microservices.Catalog.Api/Features/Courses/Course.cs
+++ src/services/catalog/VideoEducation.Microservices.Catalog.Api/Features/Courses/Course.cs
@@ -1,7 +1,8 @@
 ﻿using VideoEducation.Microservices.Catalog.Api.Features.Categories;
+using VideoEducation.Microservices.Catalog.Api.Repositories;
 
 namespace VideoEducation.Microservices.Catalog.Api.Features.Courses {
-    public class Course {
+    public class Course :BaseEntity{
         //Nullable ifadelerde muhakkak değer atanması gerekiyorsa default! , null geçilebilecekse ? konulur
         //Efcore açısından da anlam taşır
         public string Name { get; set; } = default!;
@@ -12,7 +13,10 @@ namespace VideoEducation.Microservices.Catalog.Api.Features.Courses {
         public Guid UserId { get; set; }
         //DateTime objesi zone kısmını tutmaz o yüzden global uygulama geliştiriyorsak DateTimeOffset objesiyle çalışmalıyız
         public DateTime CreatedDate { get; set; }
+        
         public Guid CategoryId { get; set; }
         public Category Category { get; set; } = default!;
+
+        public Feature Feature { get; set; } = default!;
     }
 }
---
+++ src/services/catalog/VideoEducation.Microservices.Catalog.Api/Features/Courses/Feature.cs
@@ -0,0 +1,12 @@
+﻿using VideoEducation.Microservices.Catalog.Api.Repositories;
+
+namespace VideoEducation.Microservices.Catalog.Api.Features.Courses
+{
+    //MongoDb kullandığımız için ekstradan Id kolonuna gerek yok,obje direkt olarak Course içerisinde obje olarak tutulacak
+    public class Feature
+    {
+        public int Duration { get; set; }
+        public float Rating { get; set; }
+        public string EducateorFullName { get; set; } = default!;
+    }
+}
